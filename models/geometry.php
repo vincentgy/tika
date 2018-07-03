376f55518ecfee7380e52fa86102700f04fef690
Vincent Guo <vg@mega.co.nz>
2018-07-03 21:15:59 +0800

    file renaming.

---
+++ models/geometry.php
@@ -0,0 +1,55 @@
+ <?php
+ include("models/location.php");
+ 
+ class Geometry {
+    static $google_api_key = 'AIzaSyBHfx31ShSJ07tKLT9W-YcmR5DCTKtcdSI';
+    static function covertToLocation($address) {
+        // Get lat and long by address         
+        $prepAddr = str_replace(' ','+',$address);
+        $geocode=file_get_contents('https://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
+        $output= json_decode($geocode);
+
+        $latitude = $output->results[0]->geometry->location->lat;
+        $longitude = $output->results[0]->geometry->location->lng;
+        $location = new Location($latitude, $longitude);
+        return $location;
+    }
+
+    static function covertToAddress($lat, $lng) {
+        // Get lat and long by address
+        $geoaddress=file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$lat.','.$lng.'&key='.Geometry::$google_api_key);
+        $output= json_decode($geoaddress);
+        if ($output->status !== "OK") {
+            return false;
+        }
+        $formatted_address = $output->results[0]->formatted_address;
+        $address_components = $output->results[0]->address_components;
+        $country = '';
+        $city = '';
+        foreach($address_components as $com) {
+            if ($com->types[0] === 'locality') {
+                $city = $com->long_name;
+            }
+            if ($com->types[0] === 'country') {
+                $country = $com->long_name;
+            }
+        }
+        $address = new Address($formatted_address);
+        $address->country = $country;
+        $address->city = $city;
+        return $address;
+    }
+
+    static function distance($lat1, $lon1, $lat2, $lon2) {
+
+        $theta = $lon1 - $lon2;
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $dist = $dist * 60 * 1.1515*1.609344;
+
+        return $dist;
+    }
+}
+?>
+
---
+++ models/user.php
@@ -0,0 +1,82 @@
+<?php
+require_once "../config.php";
+
+class USER
+{
+
+    static function checkuser($email, $password) {
+        $sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+        $r = false;
+
+        if($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+            if(mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1) {
+                    mysqli_stmt_close($stmt);
+                    $r = true;
+                }
+            }
+            else {
+                echo("Error description: " . mysqli_error($con));
+                mysqli_stmt_close($stmt);
+            }
+        }
+        mysqli_close($link);
+        return $r;
+    }
+
+    static function checkemail($email) {
+        $sql = "SELECT * FROM users WHERE email = ?";
+        $r = false;
+
+        if($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            if(mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1) {
+                    mysqli_stmt_close($stmt);
+                    $r = true;
+                }
+            }
+            else {
+                echo("Error description: " . mysqli_error($con));
+                mysqli_stmt_close($stmt);
+            }
+        }
+        mysqli_close($link);
+        return $r;
+    }
+
+    static function adduser($email, $password)
+    {
+        if (USER::checkemail($email) === true) {
+            return false;
+        }
+        $r = false;
+
+        $sql = "INSERT INTO users (email, password, lastlogin) VALUES (?,?, UNIX_TIMESTAMP())";
+
+        if($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+            if(mysqli_stmt_execute($stmt)){
+                $r = true;
+            }
+            else {
+                $r = false;
+            }
+            mysqli_stmt_close($stmt);
+        }
+        else {
+            echo("Error description: " . mysqli_error($con));
+            $r = false;
+        }
+        mysqli_close($link);
+        return $r;
+    }
+}
+?>
