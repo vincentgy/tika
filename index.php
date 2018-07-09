78e04cb70325782659655abc266fcb9f80c38439
Vincent Guo <vg@mega.co.nz>
2018-07-09 23:10:20 +0800

    fix a typo.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once "./models/job.php";
 
 $response = array();
 $data = json_decode(file_get_contents('php://input'), true);
-error_log(print_r($data));
+error_log(print_r($data, true));
 $req = $data["param"];
 
 if(isset($req['a'])) {
