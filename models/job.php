9e570fce819c0a5535a1a4f2384db851187c407a
Vincent Guo <vg@mega.co.nz>
2018-08-21 16:01:03 +0800

    remove typo in job search.

--- models/job.php
+++ models/job.php
@@ -223,8 +223,6 @@ class JOB
         $sql = JOB::generatesql($query);
         error_log(print_r($sql, true));
         if($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $region_id);
             if(mysqli_stmt_execute($stmt)) {
                 $result = mysqli_stmt_get_result($stmt);
 
