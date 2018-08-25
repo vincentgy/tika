d9a2066dca4ec35a803d6c4d7537359c482d6ebf
Vincent Guo <vg@mega.co.nz>
2018-08-25 17:27:18 +0800

    check fileToUpload first.

--- upload.php
+++ upload.php
@@ -21,53 +21,58 @@ else if ($category === 'c') {
 	$folder = 'companies/';
 }
 
-$file = $_FILES["fileToUpload"]['tmp_name'];
-$ext = pathinfo($_FILES["fileToUpload"]['name'], PATHINFO_EXTENSION);
-$keyName = $folder . $id . '.' . $ext;
-	// Add it to S3
-	try {
-		// Uploaded:
-		$s3->putObject(
-			array(
-				'Bucket'=>$bucketName,
-				'Key' =>  $keyName,
-				'SourceFile' => $file,
-				'ACL'          => 'public-read',
-				'StorageClass' => 'REDUCED_REDUNDANCY'
-			)
-		);
-	} catch (S3Exception $e) {
-		$response['ret'] = 1;
-		$response['error'] = $e->getMessage();
-	} catch (Exception $e) {
-		$response['ret'] = 1;
-		$response['error'] = $e->getMessage();
+if (isset($_FILES["fileToUpload"])) {
+	$file = $_FILES["fileToUpload"]['tmp_name'];
+	//$ext = pathinfo($_FILES["fileToUpload"]['name'], PATHINFO_EXTENSION);
+	$keyName = $folder . $id;
+		// Add it to S3
+		try {
+			// Uploaded:
+			$s3->putObject(
+				array(
+					'Bucket'=>$bucketName,
+					'Key' =>  $keyName,
+					'SourceFile' => $file,
+					'ACL'          => 'public-read',
+					'StorageClass' => 'REDUCED_REDUNDANCY'
+				)
+			);
+		} catch (S3Exception $e) {
+			$response['ret'] = 1;
+			$response['error'] = $e->getMessage();
+		} catch (Exception $e) {
+			$response['ret'] = 1;
+			$response['error'] = $e->getMessage();
+		}
+		// Now that you have it working, I recommend adding some checks on the files.
+		// Example: Max size, allowed file types, etc.
+
+
+	$url = htmlEntities('https://'. $bucketName . '.s3.us-east-2.amazonaws.com/'. $keyName);
+	$sql = false;
+	if ($category === 'u') {
+		$sql = 'UPDATE users SET avatar = ? WHERE id = ?';
 	}
-	// Now that you have it working, I recommend adding some checks on the files.
-	// Example: Max size, allowed file types, etc.
-
-
-$url = htmlEntities('https://'. $bucketName . '.s3.us-east-2.amazonaws.com/'. $keyName);
-$sql = false;
-if ($category === 'u') {
-	$sql = 'UPDATE users SET avatar = ? WHERE id = ?';
-}
-if($stmt = mysqli_prepare($link, $sql)) {
-    mysqli_stmt_bind_param($stmt, "ss", $url, $id);
-    if(mysqli_stmt_execute($stmt)){
-        $response['ret'] = 0;
-        $response['url'] = $url;
-    }
-    else {
-        $response['ret'] = 1;
-    }
-    mysqli_stmt_close($stmt);
+	if($stmt = mysqli_prepare($link, $sql)) {
+	    mysqli_stmt_bind_param($stmt, "ss", $url, $id);
+	    if(mysqli_stmt_execute($stmt)){
+	        $response['ret'] = 0;
+	        $response['url'] = $url;
+	    }
+	    else {
+	        $response['ret'] = 1;
+	    }
+	    mysqli_stmt_close($stmt);
+	}
+	else {
+		$response['ret'] = 1;
+	    $response['error'] = mysqli_error($link);
+	}
 }
 else {
-	$response['ret'] = 1;
-    $response['error'] = mysqli_error($link);
+	$response['ret'] = -2;
+	$response['error'] = 'Undefined fileToUpload';
 }
-
 header('Content-Type: application/json');
 echo json_encode($response);
 ?>
