e36fc204f5008cd6b461daad7abce0e4c9e7d4b9
Vincent Guo <vg@mega.co.nz>
2018-09-07 12:00:52 +0800

    fix user_id when fetching chat list.

--- models/chat.php
+++ models/chat.php
@@ -103,7 +103,7 @@ class CHAT
 
         if($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $chat_id);
+            mysqli_stmt_bind_param($stmt, "i", $user_id);
             if(mysqli_stmt_execute($stmt)) {
                 $result = mysqli_stmt_get_result($stmt);
                 while ($row=mysqli_fetch_assoc($result)) {
