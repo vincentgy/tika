20d0b025b3f56466666ea3b0736d20e5f818bf26
Vincent Guo <vg@mega.co.nz>
2018-09-09 22:07:58 +0800

    add hex dump.

--- wsserver.php
+++ wsserver.php
@@ -89,6 +89,35 @@ function pack16le($x) {
     return $r;
 };
 
+function hex_dump($data, $newline="\n")
+{
+  static $from = '';
+  static $to = '';
+
+  static $width = 16; # number of bytes per line
+
+  static $pad = '.'; # padding for non-visible characters
+
+  if ($from==='')
+  {
+    for ($i=0; $i<=0xFF; $i++)
+    {
+      $from .= chr($i);
+      $to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+    }
+  }
+
+  $hex = str_split(bin2hex($data), $width*2);
+  $chars = str_split(strtr($data, $from, $to), $width);
+
+  $offset = 0;
+  foreach ($hex as $i => $line)
+  {
+    echo sprintf('%6X',$offset).' : '.implode(' ', str_split($line,2)) . ' [' . $chars[$i] . ']' . $newline;
+    $offset += $width;
+  }
+}
+
 function unpack16le($x) {
     $r = 0;
 
@@ -134,7 +163,7 @@ function cmd($msg) {
         $buf .= $msg['message'];
     break;
     }
-    echo bin2hex($buf);
+    hex_dump($buf);
     return $buf;
 }
 
