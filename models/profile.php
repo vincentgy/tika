2d110df875d8235d709c5da67e891a63e6db6804
Yong Guo <vincentgy@Yongs-MacBook-Pro.local>
2018-09-19 20:39:17 +0800

    fix typos.

--- models/profile.php
+++ models/profile.php
@@ -63,7 +63,7 @@ class PROFILE
 
         if($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $id);
+            mysqli_stmt_bind_param($stmt, "i", $userid);
             if(mysqli_stmt_execute($stmt)) {
                 $result = mysqli_stmt_get_result($stmt);
 
@@ -135,7 +135,7 @@ class PROFILE
 
         if($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $id);
+            mysqli_stmt_bind_param($stmt, "i", $userid);
             if(mysqli_stmt_execute($stmt)) {
                 $result = mysqli_stmt_get_result($stmt);
 
